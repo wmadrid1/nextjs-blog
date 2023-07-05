1859d859b3326b94f7fff5cb313dfc2ef19b09f6
Wilson Madrid <wmadrid@nolo.com>
2023-07-04 22:24:12 -0500

    nextjs demo app

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout';
+import Head from 'next/head';
+
+export default function Custom404() {
+    return (
+      <Layout>
+        <Head>
+            <title>404 - Page Not Found</title>
+        </Head>
+
+            <h1>404 - Page Not Found</h1>
+        </Layout>
+    );
+  }
